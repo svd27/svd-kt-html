5c224843bf8dc675cdb64e4aafeb5eb7cd2da922
svd <sasa.djuric@swissbusinet.ch>
2013-08-15 23:13:27 +0200

    bla

--- utility/src/main/kotlin/ch/passenger/kotlin/util/pdf/krazydad.kt
+++ utility/src/main/kotlin/ch/passenger/kotlin/util/pdf/krazydad.kt
@@ -1,5 +1,24 @@
 package ch.passenger.kotlin.util.pdf
 
+import org.apache.pdfbox.PDFReader
+import org.apache.pdfbox.pdmodel.PDDocument
+import java.io.File
+import org.apache.pdfbox.pdmodel.PDPageNode
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDDocumentOutline
+
+
 /**
  * Created by sdju on 14.08.13.
  */
+fun main(args:Array<String>) {
+    val doc : PDDocument = PDDocument.load(File("D:/dev/svd/proj/kotlin/svd-kt-html/test/KD_Sudoku_CH_8_v1.pdf"))!!
+
+    val pages = doc.getDocumentCatalog()!!.getPages()!!
+
+    pages.getKids()?.forEach {
+        println(it)
+        val pn = it as PDPageNode
+        pn.getResources()
+    }
+
+}
